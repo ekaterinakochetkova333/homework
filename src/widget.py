fd0e3fc0b8135a989e1a72687d0ecaf493515ad6
kochetkova <salomatova2021@inbox.ru>
2025-04-26 16:58:18 +0700

    modified src/widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,5 @@
-from audioop import reverse
+
 from datetime import datetime
-from operator import index
 
 from src.masks import get_mask_account, get_mask_card_number
 
@@ -10,17 +9,15 @@ def mask_account_card(card: str) -> str:
 
     check = "Счет "
     if check in card:
-        new_mask = get_mask_card_number(card[len(check):])
+        new_mask = get_mask_account(card[len(check):])
         return check + new_mask
-        # return f"{card[:5]}**{card:[-4:]}"
-    else:
-        reverse_str = str[::-1]
-        index_space = reverse_str.find(" ")
-        index_space = len(str) - index_space
-        first_text = str[:index_space]
-        return first_text + get_mask_account(str[index_space:])
 
-        # return f"{card[:-12]} {card[-12:-10]}** **** {card[-4:]}"
+    else:
+        reverse_str = card[::-1]
+        index_space = reverse_str.find(" ")
+        index_space = len(card) - index_space
+        first_text = card[:index_space]
+        return first_text + get_mask_card_number(card[index_space:])
 
 
 def get_data(data: str) -> str:
