fdd3ad8ac9decb294a7a597d6825be6423a98f5b
kochetkova <salomatova2021@inbox.ru>
2025-05-20 15:44:54 +0700

    add new file

--- src/masks.py
+++ src/masks.py
@@ -4,7 +4,7 @@ def get_mask_card_number(card_number: str) -> str:
     if card_number.isdigit() and len(card_number) == 16:
         return f"{card_number[:4]} {card_number[4:6]}{'*' * 2} {'*' * 4} {card_number[12:]}"
     else:
-        return ""
+        return "Неверный номер карты"
 
 
 def get_mask_account(bank_account: str) -> str:
@@ -13,4 +13,4 @@ def get_mask_account(bank_account: str) -> str:
     if bank_account.isdigit() and len(bank_account) == 20:
         return f"{'*' * 2}{bank_account[-4::]}"
     else:
-        return ""
+        return "Неверный номер счета"
