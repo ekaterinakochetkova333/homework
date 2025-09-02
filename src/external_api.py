b13833bd8474937ee5ea3a3ca5a56f258a61a3d6
kochetkova <salomatova2021@inbox.ru>
2025-09-02 20:16:23 +0700

    fl8

--- src/external_api.py
+++ src/external_api.py
@@ -1,4 +1,8 @@
 import requests
+import os
+import dotenv
+
+dotenv.load_dotenv()
 
 
 def get_transaction_amount_rub(transaction) -> float:
@@ -11,7 +15,7 @@ def get_transaction_amount_rub(transaction) -> float:
     elif currency_code == "USD" or currency_code == "EUR":
         url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency_code}&amount={amount}"
         payload = {}
-        headers = {"apikey": "24z0Oh9LjLrDGi9p8XyNuCwZ3k6HvEz4"}
+        headers = {"apikey": os.getenv('API_KEY')}    #"24z0Oh9LjLrDGi9p8XyNuCwZ3k6HvEz4"
         response = requests.request("GET", url, headers=headers, data=payload)
         status_code = response.status_code
         result = response.json()
