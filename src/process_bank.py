22ffdbe92ff20ba3ed5cc63c8924c1aaaff0c2be
kochetkova <salomatova2021@inbox.ru>
2025-10-19 06:35:16 +0700

    new file

--- src/process_bank.py
+++ src/process_bank.py
@@ -1,15 +1,10 @@
 import re
-
+from collections import Counter
 
 def process_bank_search(data: list[dict], search: str) -> list[dict]:
-    return [i for i in data if re.search(search, i.get("description"))]
+    return [i for i in data if re.search(search, i.get("description"), re.IGNORECASE)]
 
 
 def process_bank_operations(data: list[dict], categories: list) -> dict:
-    counts = {category: 0 for category in categories}
-    for operation in data:
-        desc = operation.get('description', None)
-        if desc is not None and desc in categories:
-            counts[desc] += 1
-
-    return counts
+    counted = Counter(map(lambda x:x.get("description"), data))
+    return {k:v for k, v in counted.items() if k in categories}
